f9378e5b28fb40d2e4a24e3f9bfd0da643bed60e
skandermez <meziane.skander@outlook.fr>
2020-09-07 10:47:13 +0100

    adding user to python script

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
-from elasticsearch import Elasticsearch
 import sys
+from elasticsearch import Elasticsearch
+
 index = sys.argv[1]
 ci_cd = str(sys.argv[2])
 
 elastic_client = Elasticsearch(hosts=["localhost"])
 
 # Get last data
-query_body ={
+last_record_query_body ={
     "size": 1,
     "sort": {"@timestamp": "desc"},
     "query": {
@@ -14,23 +15,52 @@ query_body ={
     }
 }
 
+get_build_user_query_body = {
+    "query": {
+        "match_phrase": {
+            "message" : "build started by "
+        }
+    }
+}
+
+
+build_user_result = elastic_client.search(
+    index=index,
+    body=get_build_user_query_body)
+build_user = build_user_result['hits']['hits'][0]['_source']['message'][0]
+
+
+tmp = build_user.split()
+username = tmp[4]
+
+
 #Get last data id
 result = elastic_client.search(
     index=index,
-    body=query_body)
-print ( "message  : ")
-print (result['hits']['hits'][0]['_source']['message'])
-print ( "id  : ")
+    body=last_record_query_body)
+
 documentId = result['hits']['hits'][0]['_id']
-print (documentId)
 
-
-#adding new attribute for CI/CD
-r = elastic_client.update(index=index , id=documentId, body={"script":{
+add_ci_cd_body = {
+    "script":{
     "source": "ctx._source.ci_cd = params.ci_cd",
     "lang": "painless",
     "params" : {
         "ci_cd": ci_cd
-    }
+    }}
 }
-})
+
+add_user_name_body = {
+"script":{
+    "source": "ctx._source.username = params.username",
+    "lang": "painless",
+    "params" : {
+        "username": username
+    }
+}}
+
+
+#adding new attribute for CI/CD
+elastic_client.update(index=index , id=documentId, body=add_ci_cd_body)
+elastic_client.update(index=index , id=documentId, body=add_user_name_body)
+
